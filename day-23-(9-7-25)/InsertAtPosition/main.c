5ab4d9846a905e018d43e766411fe2aa4dcc5e74
latchiya-0717 <latchiya.d.07@gmail.com>
2025-07-14 17:24:01 +0530

    Updated

---
+++ day-23-(9-7-25)/InsertAtPosition/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Node *head=NULL,*createNode=NULL,*newNode=NULL;
+void insert(struct Node **head, struct Node **newNode){
+    printf("Enter the number of data: ");
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the value: ");
+        int val;
+        scanf("%d",&val);
+        //create address and store it in the create node 
+        createNode = (struct Node*) malloc(sizeof(struct Node));
+        createNode->data=val;
+        createNode->next=NULL;
+         
+        if(*head==NULL)
+        {
+         *head=createNode;
+        }
+        else
+        {
+            (*newNode)->next=createNode;
+        }
+        (*newNode)=createNode;
+         
+    }
+}
+void insertAtPosition(struct Node **head){
+    struct Node *temp= *head,*createNode=NULL;
+    createNode = (struct Node*) malloc(sizeof(struct Node));
+    int position,value;
+    printf("Enter the value:");
+    scanf("%d",&value);
+    printf("Enter the position: ");
+    scanf("%d",&position);
+    createNode->data=value;
+    createNode->next=NULL;
+    if(position==1){
+        createNode->next=*head;
+        *head=createNode;
+    }
+    else{
+        for(int i=1;i<position-1;i++){
+        temp=temp->next;
+        }
+        createNode->next=temp->next;
+        temp->next=createNode;
+        
+    }
+    
+}
+void display(){
+    struct Node *itr=head;
+    while(itr!=NULL)
+    {
+        printf("%d\n",itr->data);
+        itr=itr->next;
+    }
+}
+int main()
+{
+ int choice;
+    while(1){
+        printf("\nLinked List Operations Menu:\n1. Create Linked List\n 2. Display Linked List\n 3. Insert the value at Between\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+                case 1:{
+                    insert(&head,&newNode); 
+                    break;
+                }
+                case 2:{
+                    display(&head,&newNode);
+                    break;
+                }
+                case 3:{
+                    insertAtPosition(&head);
+                    break;
+                }
+        }
+    }   
+    
+    return 0;
+}
